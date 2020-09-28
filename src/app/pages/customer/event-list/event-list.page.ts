2feee1e9aacbd7bd2f681601074990f8e6bf8c5c
Asala <asala9x@gmil.com>
2020-09-28 15:47:18 +0400

    daiatian datels done

--- src/app/pages/customer/event-list/event-list.page.ts
+++ src/app/pages/customer/event-list/event-list.page.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventListPage implements OnInit {
 
+   private event=[
+    {
+      "image":"../../../../assets/icon/event1.jpg",
+      "Name":"event Name"
+    },
+    {
+      "image":"../../../../assets/icon/event1.jpg",
+      "Name":"even Name"
+    },
+    {
+      "image":"../../../../assets/icon/event1.jpg",
+      "Name":"event Name"
+    },
+    {
+      "image":"../../../../assets/icon/event1.jpg",
+      "Name":"event Name"
+    },
+    {
+      "image":"../../../../assets/icon/event1.jpg",
+      "Name":"event Name"
+    },
+    {
+      "image":"../../../../assets/icon/event1.jpg",
+      "Name":"event Name"
+    },
+   ];
+
   constructor() { }
 
   ngOnInit() {
