3e7f37ead4d00dc71b1d0833167a6625df90de50
Fatma <Fatmaalbrashdi.1995@gmail.com>
2020-09-28 13:03:41 +0400

    add dietitian

--- src/app/pages/admin/admin-add-dietitian/admin-add-dietitian.page.ts
+++ src/app/pages/admin/admin-add-dietitian/admin-add-dietitian.page.ts
@@ -37,150 +37,150 @@ export class AdminAddDietitianPage implements OnInit {
 
   ngOnInit() {
   }
-//Method to add Dietitian to firebase
-async addDietitian() {
+  //Method to add Dietitian to firebase
+  async addDietitian() {
 
-  const loading = await this.loadingController.create({
-    message: 'Please wait...',
-  });
-  await loading.present();
-
-  if (this.dietitianObj.name == "") {
-    loading.dismiss();
-    this.alert.presentAlert("Please Enter Dietitian Name");
-  }
-
-  else if (this.dietitianObj.descripion == "") {
-    loading.dismiss();
-    this.alert.presentAlert("Please Enter Dietitian Descripion");
-  }
-  else if (this.dietitianObj.phone == "") {
-    loading.dismiss();
-    this.alert.presentAlert("Please Enter Dietitian Phone Number");
-  }
-  else if (this.dietitianObj.email == "") {
-    loading.dismiss();
-    this.alert.presentAlert("Please Enter Dietitian Email");
-  }
-  else if (this.base64Img == "../../../../assets/icon/camera.png") {
-
-    this.dietitianObj.img =  this.tempbase64Img;
-    this.afData.list("dietitian").push(this.dietitianObj).then((dataresposeobj) => {
-
-      this.afData.list("dietitian/" + dataresposeobj.key).set("dietitiankey", dataresposeobj.key).then(() => {
-        loading.dismiss();
-        this.alert.presentAlert("Dietitian data inserted successfully");
-        //this.presentAlert("Dietitian data inserted successfully");
-
-      }).catch((error) => {
-        loading.dismiss();
-        this.alert.presentAlert(error.message);
-        // this.presentAlert(storageError.message);
-      });
-
-    }).catch((storageError) => {
-      loading.dismiss();
-      this.alert.presentAlert(storageError.message);
-      // this.presentAlert(storageError.message);
+    const loading = await this.loadingController.create({
+      message: 'Please wait...',
     });
+    await loading.present();
 
-  }
-  else {
-    let filename = Math.floor(Date.now() / 1000);
-    let imagepath = filename + '.jpg';
+    if (this.dietitianObj.name == "") {
+      loading.dismiss();
+      this.alert.presentAlert("Please Enter Dietitian Name");
+    }
 
-    this.afstorage.ref(imagepath).putString(this.base64Img, 'data_url')
-      .then((storageSuccess) => {
+    else if (this.dietitianObj.descripion == "") {
+      loading.dismiss();
+      this.alert.presentAlert("Please Enter Dietitian Descripion");
+    }
+    else if (this.dietitianObj.phone == "") {
+      loading.dismiss();
+      this.alert.presentAlert("Please Enter Dietitian Phone Number");
+    }
+    else if (this.dietitianObj.email == "") {
+      loading.dismiss();
+      this.alert.presentAlert("Please Enter Dietitian Email");
+    }
+    else if (this.base64Img == "../../../../assets/icon/camera.png") {
 
-        let ref = this.afstorage.ref(imagepath);
-        ref.getDownloadURL().subscribe((url) => {
-          this.dietitianObj.img = url;
-          this.afData.list("dietitian").push(this.dietitianObj).then((dataresposeobj) => {
+      this.dietitianObj.img = this.tempbase64Img;
+      this.afData.list("dietitian").push(this.dietitianObj).then((dataresposeobj) => {
 
-            this.afData.list("dietitian/" + dataresposeobj.key).set("dietitiankey", dataresposeobj.key).then(() => {
-              loading.dismiss();
-              this.alert.presentAlert("Dietitian data inserted successfully");
-              //this.presentAlert("Dietitian data inserted successfully");
-            }).catch((error) => {
-              loading.dismiss();
-              this.alert.presentAlert(error.message);
-              //this.presentAlert(error.message);
-            });
+        this.afData.list("dietitian/" + dataresposeobj.key).set("dietitiankey", dataresposeobj.key).then(() => {
+          loading.dismiss();
+          this.alert.presentAlert("Dietitian data inserted successfully");
+          //this.presentAlert("Dietitian data inserted successfully");
 
-          }).catch((databaseError) => {
-            loading.dismiss();
-            this.alert.presentAlert(databaseError.message);
-            //this.presentAlert(databaseError.message);
-          });
-
+        }).catch((error) => {
+          loading.dismiss();
+          this.alert.presentAlert(error.message);
+          // this.presentAlert(storageError.message);
         });
 
       }).catch((storageError) => {
         loading.dismiss();
         this.alert.presentAlert(storageError.message);
         // this.presentAlert(storageError.message);
-      })
+      });
 
-  }
-}
-// Action for add image for  Advices from Camera or Gallery
-async selectDietitianImage() {
-  const actionSheet = await this.actionSheetController.create({
-    header: 'Pick Image',
-    buttons: [{
-      text: 'Camera',
-      handler: () => {
-        this.selectImageFromCamera();
-      }
-    }, {
-      text: 'Gallery',
-      handler: () => {
-        this.selectImageFromGallery();
-      }
-    }, {
-      text: 'Cancel',
-      icon: 'close',
-      role: 'cancel',
-      handler: () => {
-        console.log('Cancel clicked');
-      }
-    }]
-  });
-  await actionSheet.present();
-}
-//Method camera to  open it
-selectImageFromCamera() {
-  const options: CameraOptions = {
-    quality: 100,
-    destinationType: this.camera.DestinationType.DATA_URL,
-    encodingType: this.camera.EncodingType.JPEG,
-    mediaType: this.camera.MediaType.PICTURE
-  }
+    }
+    else {
+      let filename = Math.floor(Date.now() / 1000);
+      let imagepath = filename + '.jpg';
 
-  this.camera.getPicture(options).then((imageData) => {
-    // imageData is either a base64 encoded string or a file URI
-    // If it's base64 (DATA_URL):
-    this.base64Img = 'data:image/jpeg;base64,' + imageData;
-  }, (err) => {
-    // Handle error
-  });
-}
-//Method gallery to  open it
-selectImageFromGallery() {
-  const options: CameraOptions = {
-    quality: 100,
-    destinationType: this.camera.DestinationType.DATA_URL,
-    sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
-    encodingType: this.camera.EncodingType.JPEG,
-    mediaType: this.camera.MediaType.PICTURE
-  }
+      this.afstorage.ref(imagepath).putString(this.base64Img, 'data_url')
+        .then((storageSuccess) => {
 
-  this.camera.getPicture(options).then((imageData) => {
-    // imageData is either a base64 encoded string or a file URI
-    // If it's base64 (DATA_URL):
-    this.base64Img = 'data:image/jpeg;base64,' + imageData;
-  }, (err) => {
-    // Handle error
-  });
-}
+          let ref = this.afstorage.ref(imagepath);
+          ref.getDownloadURL().subscribe((url) => {
+            this.dietitianObj.img = url;
+            this.afData.list("dietitian").push(this.dietitianObj).then((dataresposeobj) => {
+
+              this.afData.list("dietitian/" + dataresposeobj.key).set("dietitiankey", dataresposeobj.key).then(() => {
+                loading.dismiss();
+                this.alert.presentAlert("Dietitian data inserted successfully");
+                //this.presentAlert("Dietitian data inserted successfully");
+              }).catch((error) => {
+                loading.dismiss();
+                this.alert.presentAlert(error.message);
+                //this.presentAlert(error.message);
+              });
+
+            }).catch((databaseError) => {
+              loading.dismiss();
+              this.alert.presentAlert(databaseError.message);
+              //this.presentAlert(databaseError.message);
+            });
+
+          });
+
+        }).catch((storageError) => {
+          loading.dismiss();
+          this.alert.presentAlert(storageError.message);
+          // this.presentAlert(storageError.message);
+        })
+
+    }
+  }
+  // Action for add image for  Advices from Camera or Gallery
+  async selectDietitianImage() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Pick Image',
+      buttons: [{
+        text: 'Camera',
+        handler: () => {
+          this.selectImageFromCamera();
+        }
+      }, {
+        text: 'Gallery',
+        handler: () => {
+          this.selectImageFromGallery();
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
+  //Method camera to  open it
+  selectImageFromCamera() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      this.base64Img = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Handle error
+    });
+  }
+  //Method gallery to  open it
+  selectImageFromGallery() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      this.base64Img = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Handle error
+    });
+  }
 }
