278c19186223522a40b3122f3109c590741a04e1
Fatma <Fatmaalbrashdi.1995@gmail.com>
2020-10-14 18:47:15 +0400

    updating sprint1&2 Done

--- src/app/pages/customer/event-details/event-details.page.ts
+++ src/app/pages/customer/event-details/event-details.page.ts
@@ -1,14 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController, LoadingController } from '@ionic/angular';
-//import fire DB
 import { AngularFireDatabase, AngularFireDatabaseModule } from '@angular/fire/database';
-//Alertservice
 import { AlertserviceService } from '../../../services/alertservice.service';
-//NavController
 import { NavController } from '@ionic/angular';
-//Service
 import { ServiceService } from '../../../services/service.service';
-//ActivatedRoute
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -27,39 +22,36 @@ export class EventDetailsPage implements OnInit {
   constructor(public alertController: AlertController,
     private afData: AngularFireDatabase,
     public loadingController: LoadingController,
-    private alert: AlertserviceService,
+    private alertservice: AlertserviceService,
     public navCtrl: NavController,
     private authService: ServiceService,
     private route: ActivatedRoute
   ) {
     //retrieve Data with email coustomer
     this.route.queryParams.subscribe((data) => {
-      // alert(JSON.stringify(data));
+      //alert(data.key);
       this.eventkey = data.eventkey;
-     this.retrieveDataFromFirebase();
+      this.retrieveDataFromFirebase(this.eventkey);
     });
-   }
-
-  ngOnInit() {
-   // this.retrieveDataFromFirebase();
   }
 
+  ngOnInit() {
+    // this.retrieveDataFromFirebase();
+  }
   // Method for retrieve data from firebase
-  async retrieveDataFromFirebase() {
+  async retrieveDataFromFirebase(eventkey) {
     const loading = await this.loadingController.create({
       message: 'Please wait...',
     });
     await loading.present();
-
-
-    this.afData.list('event', ref => ref.orderByChild('time')).valueChanges().subscribe((eveArray) => {
+    this.afData.list('event', ref => ref.orderByChild('eventkey').equalTo(eventkey)).valueChanges().subscribe((eveArray) => {
       loading.dismiss();
-
+      //alert(JSON.stringify(eveArray));
       this.eventArray = eveArray;
       this.tempArray = eveArray;
     }, (databaseError) => {
       loading.dismiss();
-      this.alert.presentAlert(databaseError.message);
+      this.alertservice.presentAlert(databaseError.message);
 
     })
 
@@ -85,26 +77,26 @@ export class EventDetailsPage implements OnInit {
           //alert(JSON.stringify(userdata))
           this.afData.list("event/" + this.eventkey + "/peopleregistered").push(userdata).then(() => {
             loading.dismiss();
-            this.alert.presentAlert("you have successfully registered for the event");
+            this.alertservice.presentAlert("you have successfully registered for the event");
 
           }).catch(() => {
             loading.dismiss();
-            this.alert.presentAlert("Error while registering for the event");
+            this.alertservice.presentAlert("Error while registering for the event");
           });
 
         }).catch((storageerror) => {
           loading.dismiss();
-          this.alert.presentAlert("Unable to get data from storage");
+          this.alertservice.presentAlert("Unable to get data from storage");
         })
 
 
       }).catch((error) => {
         loading.dismiss();
-        this.alert.presentAlert(error.message);
+        this.alertservice.presentAlert(error.message);
       });
     }
     else {
-      this.alert.presentAlert("No slots left for this event");
+      this.alertservice.presentAlert("No slots left for this event");
     }
   }
 }
