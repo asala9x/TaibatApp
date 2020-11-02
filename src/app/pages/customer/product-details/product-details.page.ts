e5f5a49e954d6bc4f9d4530ab843e627796f8a66
Asala <asala9x@gmil.com>
2020-11-02 23:27:13 +0400

    cheackout done

--- src/app/pages/customer/product-details/product-details.page.ts
+++ src/app/pages/customer/product-details/product-details.page.ts
@@ -17,29 +17,31 @@ import { ServiceService } from '../../../services/service.service';
 })
 export class ProductDetailsPage implements OnInit {
 
-  private matches: string[] = [];
+  // private matches: string[] = [];
   private tempArray: any[] = [];
-  private tempArray1: any[] = [];
+  // private tempArray1: any[] = [];
   qty: any;
   private productskey: string = "";
 
 
   private order = {
     "ProductName": "",
-    "price":"",
-    "qty":"",
-    "CustomerId":""
+    "price": "",
+    "qty": "",
+    "CustomerId": ""
   };
+
+
   constructor(public alertController: AlertController,
     private alert: AlertserviceService,
     private afData: AngularFireDatabase,
     public navCtr: NavController,
     public loadingController: LoadingController
-    , private route: ActivatedRoute, 
+    , private route: ActivatedRoute,
     private authService: ServiceService) {
 
     this.route.queryParams.subscribe((data) => {
-      
+
       this.productskey = data.productskey;
       this.retrieveDataFromFirebase(this.productskey);
     });
@@ -47,7 +49,7 @@ export class ProductDetailsPage implements OnInit {
   }
 
   ngOnInit() {
-    
+
   }
 
   async retrieveDataFromFirebase(productskey) {
@@ -56,12 +58,12 @@ export class ProductDetailsPage implements OnInit {
     });
     await loading.present();
 
-    
+
     this.afData.list('products', ref => ref.orderByChild("productskey").equalTo(productskey)).valueChanges().subscribe((proArray) => {
       loading.dismiss();
 
       this.tempArray = proArray;
-      
+
     }, (databaseError) => {
       loading.dismiss();
       this.alert.presentAlert(databaseError.message);
@@ -69,7 +71,7 @@ export class ProductDetailsPage implements OnInit {
 
   }
 
- 
+
 
   // increment product qty
   incrementQty() {
@@ -87,50 +89,96 @@ export class ProductDetailsPage implements OnInit {
       console.log("2->" + this.qty);
     }
   }
+
+
+  //not sure
+  viewProductData() {
+
+    this.afData.list("products/" + this.productskey + "/order").valueChanges().subscribe((suceess) => {
+
+      console.log(suceess);
+      let temp2 = suceess;
+      let temp: any[] = [];
+      for (let i = 0; i < temp2.length; i++) {
+        console.log(temp2);
+        temp.push(temp2[i]);
+      }
+
+      console.log(temp);
+
+      
+
+      console.log(suceess);
+    }, (error) => {
+
+    });
+
+
+
+  }
+
+
   //AddToCart
   async AddToCart(order) {
-   
+    let temp2;
+    let temp: any[] = [];
+
+    this.afData.list("products/" + this.productskey + "/orders").valueChanges().subscribe((suceess) => {
+
+      console.log(suceess);
+      temp2 = suceess;
+
+      for (let i = 0; i < temp2.length; i++) {
+        console.log(temp2);
+        temp.push(temp2[i]);
+      }
+
+      console.log(temp);
+
+      console.log(suceess);
+    }, (error) => {
+
+    });
+
+
     let orderObj = {
       "ProductName": this.order.ProductName,
-      "price":this.order.price,
-      "qty":this.order.qty,
-      "CustomerId":this.order.CustomerId
+      "price": this.order.price,
+      "qty": this.order.qty,
+      "CustomerId": this.order.CustomerId
 
     };
-    orderObj.ProductName=this.tempArray[0].ProductName;
-    orderObj.price=this.tempArray[0].price;
-    orderObj.qty=this.qty;
-    
+    orderObj.ProductName = this.tempArray[0].ProductName;
+    orderObj.price = this.tempArray[0].price;
+    orderObj.qty = this.qty;
+
 
     const loading = await this.loadingController.create({
       message: 'Please wait...',
     });
     await loading.present();
 
-     //this.afData.list('products').update(this.productskey, orderObj).then(() => {
-     this.authService.getDataFromStorage().then((userdata ) => {
-     
-     
-     orderObj.CustomerId=userdata.uid;
-    this.afData.list("products/" + this.productskey + "/orders").push(orderObj).then(() => {
-      
-      loading.dismiss();
-      this.alert.presentAlert("you have successfully added for the cart");
+    
+    this.authService.getDataFromStorage().then((userdata) => {
 
-      })
-      .catch(() => {
+
+      orderObj.CustomerId = userdata.uid;
+      temp.push(orderObj);
+      console.log(temp);
+      this.afData.list('/products/' + this.productskey).update("orders", temp).then((sucess) => {
         loading.dismiss();
-        this.alert.presentAlert("Error while added for the cart");
+        this.alert.presentAlert("you have successfully added for the cart");
+      }).catch((err) => {
+        loading.dismiss();
+        this.alert.presentAlert(err.message);
       });
+      loading.dismiss();
+     
 
     }).catch((storageerror) => {
       loading.dismiss();
       this.alert.presentAlert("Unable to get data from storage");
     })
-   //}).catch((error) => {
-  //     loading.dismiss();
-  //     this.alert.presentAlert(error.message);
-  //   });
-  //   // this.alert.presentAlert("No item");
-   }
+    
+  }
 }
