d0d0163ad4c3c88d5b5f4b53b390e6bf55ae4d25
Fatma <Fatmaalbrashdi.1995@gmail.com>
2020-11-22 16:09:02 +0400

    address and location are done & modfiy the ui for search bar

--- src/app/pages/customer/address/address.page.ts
+++ src/app/pages/customer/address/address.page.ts
@@ -6,51 +6,107 @@ import { LoadingController, AlertController } from '@ionic/angular';
 //imoprt Fire Database
 import { AngularFireDatabase } from '@angular/fire/database';
 //Alertservice
+//import { AlertserviceService } from '../../../services/alertservice.service';
 import { AlertserviceService } from '../../../services/alertservice.service';
+//ActivatedRoute
+import { ActivatedRoute } from '@angular/router';
+import { ServiceService } from '../../../services/service.service';
 @Component({
-  selector: 'app-address',
-  templateUrl: './address.page.html',
-  styleUrls: ['./address.page.scss'],
+    selector: 'app-address',
+    templateUrl: './address.page.html',
+    styleUrls: ['./address.page.scss'],
 })
 export class AddressPage implements OnInit {
-  private ordersObj: any = {
-    "Area": "",
-    "Street": "",
-    "HomeNumber": "",
-    "PhoneNumber":""
-  }
-  constructor(
-    private afstorage: AngularFireStorage,
-    public loadingController: LoadingController,
-    public alertController: AlertController,
-    private afData: AngularFireDatabase,
-    private alert: AlertserviceService) { }
+    private address: any = {
+        "Area": "",
+        "Street": "",
+        "HomeNumber": "",
+        "PhoneNumber": "",
+        "latitude": "",
+        "longitude": "",
+        "userId":""
+    }
+    private locationdata: any;
+    constructor(
+        private afstorage: AngularFireStorage,
+        public loadingController: LoadingController,
+        public alertController: AlertController,
+        private afData: AngularFireDatabase,
+        private alert: AlertserviceService,
+        private authService: ServiceService,
+        private route: ActivatedRoute) {
+        this.route.queryParams.subscribe((data) => {
+            //alert(JSON.stringify(data));
 
-  ngOnInit() {
-  }
-    //Method to add product to firebase
+            this.locationdata = data;
+            this.address.latitude = data.latitude;
+            this.address.longitude = data.longitude;
+        });
+
+    }
+
+    ngOnInit() {
+        this.retrieveDataFromFirebase();
+    }
+    async retrieveDataFromFirebase() {
+
+        const loading = await this.loadingController.create({
+            message: 'Please wait...',
+        });
+        await loading.present();
+
+        this.authService.getDataFromStorage().then((userdata)=>{
+           
+    
+            this.afData.object('Address/'+userdata.uid).valueChanges().subscribe((addressobj:any) => {
+    
+                loading.dismiss();
+                this.address=addressobj;
+    
+            }, (databaseError) => {
+    
+                loading.dismiss();
+                this.alert.presentAlert(databaseError.message);
+    
+            })
+        }).catch((error)=>{
+            loading.dismiss();
+            this.alert.presentAlert("Unable to get data from storage");
+        })
+
+       
+
+    }
+
+    //Method to add address to firebase
     async addOrder() {
+        //alert(this.locationdata);
+        const loading = await this.loadingController.create({
+            message: 'Please wait...',
+        });
+        await loading.present();
+        this.authService.getDataFromStorage().then((userdata) => {
+            this.address.userId=userdata.uid;
+                
+            this.afData.list("Address").set(userdata.uid,this.address).then((dataresposeobj) => {
+                // this.afData.list("Address/" + dataresposeobj.key).set("Addresskey", dataresposeobj.key).then(() => {
+                loading.dismiss();
+                this.alert.presentAlert("Address data inserted successfully");
 
-      const loading = await this.loadingController.create({
-        message: 'Please wait...',
-      });
-      await loading.present();
+                // }).catch((error) => {
+                //   loading.dismiss();
+                //   this.alert.presentAlert(error.message);
+                //   //this.presentAlert(error.message);
+                // });
 
-              this.afData.list("orders").push(this.ordersObj).then((dataresposeobj) => {
-                this.afData.list("orders/" + dataresposeobj.key).set("orderskey", dataresposeobj.key).then(() => {
-                  loading.dismiss();
-                  this.alert.presentAlert("Order data inserted successfully");
-            
-                }).catch((error) => {
-                  loading.dismiss();
-                  this.alert.presentAlert(error.message);
-                  //this.presentAlert(error.message);
-                });
-  
-              }).catch((databaseError) => {
+            }).catch((databaseError) => {
                 loading.dismiss();
                 this.alert.presentAlert(databaseError.message);
                 //this.presentAlert(databaseError.message);
-              });
-}
+            });
+        }).catch((storageerror) => {
+            loading.dismiss();
+            this.alert.presentAlert("Unable to get data from storage");
+        })
+    }
 }
--- src/app/pages/customer/location/location.page.ts
+++ src/app/pages/customer/location/location.page.ts
@@ -4,87 +4,150 @@ import { ViewChild, ElementRef } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { ActivatedRoute } from '@angular/router';
 import { AlertserviceService } from '../../../services/alertservice.service';
+//NavigationExtras
+import { NavigationExtras } from '@angular/router';
+//NavController
+import { NavController } from '@ionic/angular';
+import { LoadingController, ToastController } from '@ionic/angular';
+import { ServiceService } from '../../../services/service.service';
+import { AngularFireDatabase, AngularFireDatabaseModule } from '@angular/fire/database';
 declare var google;
 
 @Component({
-  selector: 'app-location',
-  templateUrl: './location.page.html',
-  styleUrls: ['./location.page.scss'],
+    selector: 'app-location',
+    templateUrl: './location.page.html',
+    styleUrls: ['./location.page.scss'],
 })
 export class LocationPage implements OnInit {
 
-  @ViewChild('map', { static: false }) mapElement: ElementRef;
-  map: any;
-  private data: any = {
+    @ViewChild('map', { static: false }) mapElement: ElementRef;
+    map: any;
+    private locationdata = {
+        "longitude": 0,
+        "latitude": 0
+    }
     // "source": "",
     // "destination": ""
-    "longitude": "",
-    "latitude": ""
-  };
-  directionsService = new google.maps.DirectionsService;
-  directionsDisplay = new google.maps.DirectionsRenderer;
-
-  // private userdata:any={};
 
 
-  constructor(private geolocation: Geolocation,
-    private route: ActivatedRoute,
-    private alert: AlertserviceService) {
-    // this.route.queryParams.subscribe(receiveddata=>{
-    //     this.userdata=receiveddata;
-    // });
-  }
+    directionsService = new google.maps.DirectionsService;
+    directionsDisplay = new google.maps.DirectionsRenderer;
 
-  ngOnInit() {
-
-    setTimeout(() => {
-      let latlng = { lat: 23.614328, lng: 58.545284 };
-      let mapOptions = {
-        center: latlng,
-        zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      }
-      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-      this.directionsDisplay.setMap(this.map);
-    }, 1000);
-
-  }
-
-  displayDirectionsinMap() {
-    //   this.directionsService.route({
-    //     origin: this.data.source,
-    //     destination: this.data.destination,
-    //     travelMode: 'DRIVING'
-    //   }, (response, status) => {
-    //     if (status === 'OK') {
-    //       this.directionsDisplay.setDirections(response);
-    //     } else {
-    //       window.alert('Directions request failed due to ' + status);
-    //     }
-    //   });
-
-    this.geolocation.getCurrentPosition(
-      {
-        maximumAge: 1000, timeout: 5000,
-        enableHighAccuracy: true
-      }
-    ).then((resp) => {
-      // resp.coords.latitude
-      // resp.coords.longitude
-      //alert("r succ"+resp.coords.latitude)
-      //alert(JSON.stringify( resp.coords));
-      this.alert.presentAlert("your location has been added");
-      this.data.latitude = resp.coords.latitude
-      this.data.longitude = resp.coords.longitude
-    }, er => {
-      //alert("error getting location")
-      alert('Can not retrieve Location')
-    }).catch((error) => {
-      //alert('Error getting location'+JSON.stringify(error));
-      alert('Error getting location - ' + JSON.stringify(error))
-    })
-  }
+    // private userdata:any={};
 
 
+    constructor(private geolocation: Geolocation,
+        private route: ActivatedRoute,
+        private alert: AlertserviceService, public navCtrl: NavController,
+        public loadingController: LoadingController,
+        private authService: ServiceService,
+        private afData: AngularFireDatabase,) {
+        // this.route.queryParams.subscribe(receiveddata=>{
+        //     this.userdata=receiveddata;
+        // });
+
+    }
+
+    ngOnInit() {
+
+
+        this.geolocation.getCurrentPosition(
+            {
+                maximumAge: 1000,
+                timeout: 5000,
+                enableHighAccuracy: true
+            }
+        ).then((resp) => {
+
+            this.locationdata.latitude = resp.coords.latitude;
+            this.locationdata.longitude = resp.coords.longitude;
+
+           
+                let latlng = { lat: resp.coords.latitude, lng:resp.coords.longitude };
+                let mapOptions = {
+                    center: latlng,
+                    zoom: 15,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                }
+                this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+                this.directionsDisplay.setMap(this.map);
+           
+
+
+
+        }).catch((error) => {
+            //alert('Error getting location'+JSON.stringify(error));
+            alert('Error getting location - ' + JSON.stringify(error))
+        })
+
+
+
+    }
+
+    async displayDirectionsinMap() {
+        //   this.directionsService.route({
+        //     origin: this.data.source,
+        //     destination: this.data.destination,
+        //     travelMode: 'DRIVING'
+        //   }, (response, status) => {
+        //     if (status === 'OK') {
+        //       this.directionsDisplay.setDirections(response);
+        //     } else {
+        //       window.alert('Directions request failed due to ' + status);
+        //     }
+        //   });
+
+
+        // const loading = await this.loadingController.create({
+        //     message: 'Please wait...',
+        // });
+        // await loading.present();
+
+
+
+        // //get curent user data 
+        // this.authService.getDataFromStorage().then((userdata) => {
+
+
+        //         let AddressObj = {
+        //             "userId": userdata.uid,
+        //             "latitude": this.locationdata.latitude,
+        //             "longitude": this.locationdata.longitude
+
+        //         };
+
+
+        //         this.afData.list('Address').push(AddressObj).then((ifSeccess) => {
+        //             loading.dismiss();
+
+        //             this.alert.presentAlert("your location has been added");
+
+                    let NavExtras: NavigationExtras = {
+                        queryParams: this.locationdata
+                    }
+                    this.navCtrl.navigateForward('address', NavExtras);
+
+
+        //         }).catch((Error) => {
+        //             loading.dismiss();
+        //             this.alert.presentAlert(Error.message);
+        //         });
+
+
+
+        // }).catch((storageerror) => {
+        //     loading.dismiss();
+        //     this.alert.presentAlert("Unable to get data from storage");
+        // })
+
+        
+
+
+        //send loc to address page
+
+       
+
+    }
 }
+
 
